0c3be660ffbcf958cb3a49dd6cce23c1254a0029
holcz_norbert <holcz.prog@gmail.com>
2023-08-04 09:44:51 +0200

    Project Init

---
+++ src/main/java/hu/holnor/cinemaexam/CinemaExamApplication.java
@@ -0,0 +1,13 @@
+package hu.holnor.cinemaexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaExamApplication.class, args);
+    }
+
+}
